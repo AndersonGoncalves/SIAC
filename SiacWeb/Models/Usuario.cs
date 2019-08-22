b47ed06ac59685cf48bd22bd41bf9e4c6f6f008d
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2019-08-21 23:31:45 -0300

    Model de usuario com os campos (Nome e Ativo)

---
+++ SiacWeb/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SiacWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiacWeb.Models
+{
+    [Table("AspNetUsers")]
+    public class Usuario : IdentityUser
+    {
+        [MaxLength(256, ErrorMessage = "Tamanho máximo {1} caracteres")]
+        public string Nome { get; set; }
+       
+        public SimOuNao Ativo { get; set; }
+    }
+}
